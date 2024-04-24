f3dafc39fd493f672454f5180ff7584cf26fcc7f
Ivan Ilyakov <karginux@ya.ru>
2024-04-24 21:52:09 +0300

    Updated dotfiles

--- hosts/meteora/modules/nix.nix
+++ hosts/meteora/modules/nix.nix
@@ -5,6 +5,15 @@
     
     settings = {
       auto-optimise-store = true;
+
+      # hyprland cachix
+      substituters = [
+        "https://hyprland.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
 
     gc = {
