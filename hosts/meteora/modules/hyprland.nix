fcc1cbd929a5ddb89a2cacff836884a59facc696
Ivan Ilyakov <karginux@ya.ru>
2024-04-24 21:27:21 +0300

    Updated dotfiles

--- hosts/meteora/modules/hyprland.nix
+++ hosts/meteora/modules/hyprland.nix
@@ -7,13 +7,13 @@
       enable = true;
       hidpi = false;
     };
-
-    xdg.portal = {
-      enable = true;
-      wlr.enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-hyprland
-      ];
-    };
+  };
+    
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+    ];
   };
 }
