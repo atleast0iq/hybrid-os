d39ce7e0d896bcaf3710200d49ec2f685378277f
Ivan Ilyakov <karginux@ya.ru>
2024-06-18 21:26:34 +0300

    Updated dotfiles

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "atleast0iq's NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-stable";
     
     home-manager = {
       url = "github:nix-community/home-manager";
