caafcfeb298bb78f2f86a3c3136481c49e822e3d
Ivan Ilyakov <karginux@ya.ru>
2024-10-02 09:43:57 +0300

    Updated dotfiles

--- home/iilyakov/wm/plugins.nix
+++ home/iilyakov/wm/plugins.nix
@@ -6,5 +6,15 @@
   ...
 }: {
   wayland.windowManager.hyprland.plugins = with pkgs; [
+    hyprlandPlugins.hyprexpo
   ];
+
+  wayland.windowManager.hyprland.extraConfig = ''
+    plugin {
+      hyprexpo {
+        workspace_method = first 1
+        gesture_fingers = 3
+      }
+    }
+  '';
 }
