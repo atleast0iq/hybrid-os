8cb993e97a4f934888dd21d9ac343984721c758b
Ivan Ilyakov <karginux@ya.ru>
2024-10-28 21:29:59 +0300

    Updated dotfiles

--- home/iilyakov/programs/shell.nix
+++ home/iilyakov/programs/shell.nix
@@ -51,8 +51,18 @@
     zsh = {
       enable = true;
       enableCompletion = true;
+      completionInit = ''
+        autoload bashcompinit && bashcompinit
+        autoload -Uz compinit && compinit
+        compinit
+      '';
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+      shellAliases = {
+        ls = "exa";
+        ll = "exa -l";
+        la = "exa -la";
+      };
     };
   };
 }
