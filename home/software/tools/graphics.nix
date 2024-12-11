f19f3f58b21523199bd6e9eae16851d071a4c0f5
Ivan Ilyakov <karginux@ya.ru>
2024-12-11 21:27:33 +0300

    Updated dotfiles

--- home/software/tools/graphics.nix
+++ home/software/tools/graphics.nix
@@ -1,33 +1,37 @@
 {
   pkgs,
+  pkgs-unstable,
   pkgs-2311,
   ...
 }: {
-  home.packages = with pkgs; [
-    nautilus
-    bitwarden-desktop
-    telegram-desktop
-    obsidian
-    jetbrains.pycharm-community
-    pkgs-2311.jetbrains.clion
-    pkgs-2311.jetbrains.goland
-    simple-scan
+  home.packages =
+    (with pkgs; [
+      nautilus
+      bitwarden-desktop
+      obsidian
+      jetbrains.pycharm-community
+      pkgs-2311.jetbrains.clion
+      pkgs-2311.jetbrains.goland
+      simple-scan
 
-    (lutris.override {
-      extraPkgs = pkgs: [
-        wineWowPackages.waylandFull
-        attr
-        gamescope
-        mangohud
-        libthai
-        pango
+      (lutris.override {
+        extraPkgs = pkgs: [
+          wineWowPackages.waylandFull
+          attr
+          gamescope
+          mangohud
+          libthai
+          pango
 
-        zandronum
-      ];
+          zandronum
+        ];
 
-      extraLibraries = pkgs: [
-        attr
-      ];
-    })
-  ];
+        extraLibraries = pkgs: [
+          attr
+        ];
+      })
+    ])
+    ++ (with pkgs-unstable; [
+      telegram-desktop
+    ]);
 }
