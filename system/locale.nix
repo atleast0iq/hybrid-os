d0650de0a84ddf068ce2406e8613f95db9698904
Ivan Ilyakov <karginux@ya.ru>
2025-01-19 02:37:03 +0300

    Updated dotfiles

--- system/locale.nix
+++ system/locale.nix
@@ -1,12 +1,4 @@
-{...}: let
-  c = "C.UTF-8";
-  dk = "en_DK.UTF-8";
-  gb = "en_GB.UTF-8";
-  ie = "en_IE.UTF-8";
-  ru = "ru_RU.UTF-8";
-  us = "en_US.UTF-8";
-  lang = "en_IE:en_US:en:C:ru_RU";
-in {
+{...}: {
   time.timeZone = "Europe/Moscow";
   time.hardwareClockInLocalTime = true;
 
@@ -16,22 +8,6 @@ in {
     longitude = 37.634669;
   };
 
-  i18n.defaultLocale = ie;
-  i18n.extraLocaleSettings = {
-    LANGUAGE = lang;
-    LC_TIME = dk;
-    LC_ADDRESS = ru;
-    LC_MONETARY = ru;
-    LC_NUMERIC = ru;
-    LC_PAPER = ru;
-    LC_TELEPHONE = ru;
-  };
-  i18n.supportedLocales = map (x: "${x}/UTF-8") [
-    c
-    dk
-    gb
-    ie
-    ru
-    us
-  ];
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings.LC_TIME = "en_GB.UTF-8";
 }
