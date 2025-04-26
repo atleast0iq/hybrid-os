67dac187deb0d8ccfb2c0f000bb5ec55f9859b3d
Ivan Ilyakov <karginux@ya.ru>
2025-04-26 09:33:59 +0300

    Updated dotfiles

--- home/wm/hyprland/settings.nix
+++ home/wm/hyprland/settings.nix
@@ -11,7 +11,7 @@
     settings = {
       monitor = [
         "eDP-1,1920x1080@60,0x0,1"
-        "HDMI-A-1,1920x1080,1920x0,1"
+        "HDMI-A-1,1920x1080,-1920x0,1"
       ];
 
       workspace = [
