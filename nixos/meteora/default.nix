94e44d7a608065f19927001b87cff849eeb3feb1
Ivan Iliakov <karginux@ya.ru>
2025-09-08 01:07:54 +0300

    meow2

--- nixos/meteora/default.nix
+++ nixos/meteora/default.nix
@@ -1,6 +1,14 @@
-{
+{ self, ... }: let
+  sysmod = "${self}/modules/system"
+in {
   imports = [
     ./configuration.nix
     ./hardware-configuration.nix
+
+    "${sysmod}/nix.nix"
   ];
+
+  boot = {
+    kernelModules = ["amdgpu" "v4l2loopback" "ideapad_laptop"];
+  };
 }
